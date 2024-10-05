2af0241c81c5f3810dcc59fd1b37c05b0f55beca
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-05 21:30:03 +0530

    bugs

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const loadAllPhones = async (status, searchText) => {
     }`
   );
   const data = await responce.json();
-  //   console.log(data);
+  console.log(data);
 
   if (status) {
     displayAllPhones(data.data);
@@ -19,12 +19,40 @@ const loadAllPhones = async (status, searchText) => {
 };
 // handelShowAll
 const handelShowAll = () => {
-  //   console.log("hello");
+  console.log("hello");
   loadAllPhones(true);
 };
 // display phones
 const displayAllPhones = (phones) => {
-  //   console.log(phones);
+  console.log(phones);
+  const phonesContainer = document.getElementById("phonesContainer");
+  for (const phone of phones) {
+    console.log(phone);
+    const cards = document.createElement("div");
+    cards.innerHTML = `
+ <div class="card card-compact bg-base-100 w-96 shadow-xl">
+  //       <figure>
+  //         <img
+  //           src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+  //           alt="Shoes" />
+  //       </figure>
+  //       <div class="card-body">
+  //         <h2 class="card-title">Shoes!</h2>
+  //         <p>If a dog chews shoes whose shoes does he choose?</p>
+  //         <div class="card-actions justify-end">
+  //           <button class="btn btn-primary">Buy Now</button>
+  //         </div>
+  //       </div>
+  //     </div>
+
+`;
+  }
+  phonesContainer.appendChild(cards);
+  //
+  //
+  //     `);
+  //         phonesContainer.appendChild(card);
+  //       });
 };
 
 const handelSearch = () => {
@@ -32,7 +60,7 @@ const handelSearch = () => {
   document.getElementById("spinner").style.display = "block";
   //   inputbox work
   const searchText = document.getElementById("searchBox");
-  searchText.value;
+  searchText.classList.value;
   console.log("searchText");
   setTimeout(function () {
     loadAllPhones(false, searchText);
