fad59504ed8f65a3a882c0f0605c0b266d015f42
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-11 18:10:21 +0530

    modal done

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const fetchPhones = async (status, inputValue) => {
     } `
   );
   const data = await responce.json();
-  console.log(data);
+  // console.log(data);
   if (status) {
     const phonesArr = data.data;
     // console.log(phonesArr);
@@ -53,7 +53,7 @@ const displayPhones = (phonesArr) => {
             <h2 class="card-title">${phone_name}</h2>
             <p>${brand}</p>
             <div class="card-actions justify-end">
-              <button class="btn btn-primary text-white">Show details</button>
+              <button id="showDetails" onclick = "phoneDetails('${slug}')" class="btn btn-primar bg-none">Show details</button>
             </div>
           </div>
         </div>
@@ -67,6 +67,43 @@ const showAll = () => {
   fetchPhones(true);
 };
 
+// model action
+const phoneDetails = async (slug) => {
+  console.log(slug);
+  const responce = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${slug}`
+  );
+  const data = await responce.json();
+  console.log(data.data);
+  const { name, releaseDate, image, brand } = data.data;
+  const { storage, displaySize, chipSet, memory } = data.data.mainFeatures;
+  console.log(storage);
+  const modalContainer = document.getElementById("modalContainer");
+  modalContainer.innerHTML = `<dialog id="my_modal_1" class="modal">
+          <div class="modal-box grid grid-cols-2 ">
+           <div class="">
+           <img  src="${image}" alt="">
+          </div>
+          <div>
+            <h3 class="text-xl font-bold">${name}</h3>
+            <p class ="border-1 py-1">${brand}</p>
+           <p>${releaseDate}</p>
+            <p class="">${storage}</p>
+            <p class="">${chipSet}</p>
+            <p class="">${displaySize}</p>
+            <p class="">${memory}</p>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn">Close</button>
+              </form>
+            </div>
+            </div>
+           
+          </div>
+        </dialog>`;
+  my_modal_1.showModal();
+};
 // const arr = [12, 40, 43, 53, "raj", "rohim", "kiron"];
 // const [my_age, n2, ...other] = arr;
 // console.log(my_age);
